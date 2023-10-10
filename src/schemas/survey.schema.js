977ae8f8b4f4ff29b070caaaa7f4d49624e6ee54
leonardo <leonardogtaquini@gmail.com>
2023-10-10 14:10:18 -0300

    se corrigió los modelos de validaciones

--- src/schemas/survey.schema.js
+++ src/schemas/survey.schema.js
@@ -5,12 +5,12 @@ export const surveySchema = [
     body('idGender').notEmpty().withMessage('idGender is required'),
     body('idStudy').notEmpty().withMessage('idStudy is required'),
     body('age').notEmpty().withMessage('Age is required'),
-    body('questionsOne').notEmpty().withMessage('QuestionsOne is required'),
-    body('questionTwo').notEmpty().withMessage('QuestionTwo is required'),
-    body('questionThree').notEmpty().withMessage('QuestionThree is required'),
-    body('questionFour').notEmpty().withMessage('QuestionFour is required'),
-    body('questionFive').notEmpty().withMessage('QuestionFive is required'),
-    body('questionSix').notEmpty().withMessage('QuestionSix is required'),
-    body('questionSeven').notEmpty().withMessage('QuestionSeven is required'),
-    body('questionEight').notEmpty().withMessage('QuestionEight is required')
+    body('answerOne').notEmpty().withMessage('answerOne is required'),
+    body('answerTwo').notEmpty().withMessage('answerTwo is required'),
+    body('answerThree').notEmpty().withMessage('answerThree is required'),
+    body('answerFour').notEmpty().withMessage('answerFour is required'),
+    body('answerFive').notEmpty().withMessage('answerFive is required'),
+    body('answerSix').notEmpty().withMessage('answerSix is required'),
+    body('answerSeven').notEmpty().withMessage('answerSeven is required'),
+    body('answerEight').notEmpty().withMessage('answerEight is required')
 ]
