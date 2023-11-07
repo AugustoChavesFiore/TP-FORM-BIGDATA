80b9113e91330d034019439f16b645b08bb00995
leonardo <leonardogtaquini@gmail.com>
2023-11-07 10:49:15 -0300

    app lista

--- src/ddbb.js
+++ src/ddbb.js
@@ -1,11 +1,8 @@
 import { Sequelize } from 'sequelize';
 
 export const sequelize= new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_URL,
     {
-        host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT 
     });  
 
